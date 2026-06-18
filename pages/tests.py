64adc15b3c7acddc02887b6de9fa00bdc847ddf8
Pavel <pavelaleshin86@gmail.com>
2026-06-18 11:54:16 +0300

    Временно отключена форма обратной связи

--- pages/tests.py
+++ pages/tests.py
@@ -9,27 +9,12 @@ class ContactViewTests(TestCase):
         response = self.client.get(reverse("contact"))
         self.assertEqual(response.status_code, 200)
 
-    def test_contact_post_valid_creates_message(self):
+    def test_contact_post_does_not_create_message(self):
         response = self.client.post(
             reverse("contact"),
             {
                 "name": "Иван",
                 "email": "ivan@example.com",
-                "message": "Нужен сайт",
-            },
-        )
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(ContactMessage.objects.count(), 1)
-        msg = ContactMessage.objects.get()
-        self.assertEqual(msg.name, "Иван")
-        self.assertEqual(msg.email, "ivan@example.com")
-
-    def test_contact_post_invalid_email_does_not_create_message(self):
-        response = self.client.post(
-            reverse("contact"),
-            {
-                "name": "Иван",
-                "email": "",
                 "message": "Нужен сайт",
             },
         )
